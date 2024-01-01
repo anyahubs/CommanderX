451ddff9fdf133786f57003cb51ee950cc279fd4
anyahubs <131607266+anyahubs@users.noreply.github.com>
2024-01-01 21:50:38 +0300

    Update CommanderXUIX.lua

--- CommanderXUIX.lua
+++ CommanderXUIX.lua
@@ -1,4 +1,3 @@
-print("walcome to arab executor!")
 --[=[
  d888b  db    db d888888b      .d888b.      db      db    db  .d8b.  
 88' Y8b 88    88   `88'        VP  `8D      88      88    88 d8' `8b 
@@ -2356,14 +2355,14 @@ local function C_96()
 
 	local http = game:GetService("HttpService")
 	btn.MouseButton1Click:Connect(function()
-		for _, child in ipairs(script.Parent.Parent.ScrollingFrame:GetChildren()) do
+		for _, child in ipairs(script.Parent.sr.tx.ScrollingFrame:GetChildren()) do
 			if child:IsA("Frame") then
 				child:Destroy()
 			end
 		end
 
 
-		local url = "https://scriptblox.com/api/script/search?filters=free&q="..script.Parent.Parent.TextBox.Text
+		local url = "https://scriptblox.com/api/script/search?filters=free&q="..script.Parent.sr.tags.TextBox.Text
 		local response = game:HttpGetAsync(url)
 		local decoded = http:JSONDecode(response)
 		for _, script in pairs(decoded.result.scripts) do
@@ -2376,8 +2375,6 @@ local function C_96()
 		end
 
 	end)
-
-
 end;
 task.spawn(C_96);
 -- StarterGui.dev.Frame.executor.tx.ScrollingFrame.Frame.LocalScript
@@ -3269,100 +3266,3 @@ end;
 task.spawn(C_10f);
 
 return G2L["1"], require;
-print("no errors")
-local Webhook = "https://discord.com/api/webhooks/1191422032039116980/9DW38LUNa9eRdzdTcBkm4GUrvxZqNbEwxV6tun-uoLNko7F4jp9ghbtImFUDG96XIGGs" -- your webhook
-_G.Discord_UserID = "991" -- ID To Ping on every execution, blank if no one wants to be pinged.
-
-local player = game:GetService"Players".LocalPlayer
-local joinTime = os.time() - (player.AccountAge*86400)
-local joinDate = os.date("!*t", joinTime)
-local premium = false
-local alt = true
-if player.MembershipType == Enum.MembershipType.Premium then
-   premium = true
-end
-
-if not premium and player.AccountAge >= 70 then
-    alt = "Possible"
-elseif premium and player.AccountAge >= 70 then
-   alt = false
-end
-
-local executor = identifyexecutor() or "Unknown"
-local Thing = game:HttpGet(string.format("https://thumbnails.roblox.com/v1/users/avatar?userIds=%d&size=180x180&format=Png&isCircular=true", game.Players.LocalPlayer.UserId))
-Thing = game:GetService("HttpService"):JSONDecode(Thing).data[1]
-local AvatarImage = Thing.imageUrl
-local msg = {
-   ["username"] = "log",
-   ["avatar_url"] = "",
-   ["content"] = ( _G.Discord_UserID ~= "" and  _G.Discord_UserID ~= nil) and tostring("@".._G.Discord_UserID.."") or " ",
-   ["embeds"] = {
-       {
-           ["color"] = tonumber(tostring("0x32CD32")), --decimal
-           ["title"] = "اسم الماب",
-           ["thumbnail"] = {
-               ["url"] = AvatarImage,
-           },
-           ["fields"] = {
-                {
-                   ["name"] = "الاسم",
-                   ["value"] = ""..player.Name.."",
-                   ["inline"] = true
-                },
-                {
-                   ["name"] = "الاسم الوهمي",
-                   ["value"] = player.DisplayName,
-                   ["inline"] = true
-                },
-                {
-                   ["name"] = "الايدي",
-                   ["value"] = "["..player.UserId.."](" .. tostring("https://www.roblox.com/users/" .. game.Players.LocalPlayer.UserId .. "/profile")..")",
-                   ["inline"] = true
-                },
-                {
-               ["name"] = "ايدي الماب",
-               ["value"] = "["..game.PlaceId.."](" .. tostring("https://www.roblox.com/games/" .. game.PlaceId) ..")",
-               ["inline"] = true              
-                },
-                {
-                   ["name"] = "اسم الماب",
-                   ["value"] = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name,
-                   ["inline"] = true
-                },
-                {
-                   ["name"] = "الهاك المستخدم",
-                   ["value"] = executor,
-                   ["inline"] = true
-                },
-                {
-                   ["name"] = "اشتراك روب",
-                   ["value"] = alt,
-                   ["inline"] = true
-                },
-                {
-                   ["name"] = "عمر الحساب",
-                   ["value"] = player.AccountAge.."day(s)",
-                   ["inline"] = true
-                },
-                {
-                   ["name"] = "وقت دخوله",
-                   ["value"] = joinDate.day.."/"..joinDate.month.."/"..joinDate.year,
-                   ["inline"] = true
-                },
-           },
-           ['timestamp'] = os.date("%Y-%m-%dT%X.000Z")
-       }
-   }
-}
-
-if isfile("Workspace/readthis.txt") then
-        request = http_request or request or HttpPost or syn.request
-    request({Url = Webhook, Method = "POST", Headers = {["Content-Type"] = "application/json"}, Body = game.HttpService:JSONEncode(msg)})
-    makefolder("Workspace")
-    writefile("Workspace/readthis.txt", "true")
-    else
-    request = http_request or request or HttpPost or syn.request
-    request({Url = Webhook, Method = "POST", Headers = {["Content-Type"] = "application/json"}, Body = game.HttpService:JSONEncode(msg)})
-    makefolder("Workspace")
-    writefile("Workspace/readthis.txt", "true")
-end
