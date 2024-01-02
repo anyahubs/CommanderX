2e8be5bf2e0d7c56506749c605dfa77b6f8fecf3
anyahubs <131607266+anyahubs@users.noreply.github.com>
2024-01-02 20:18:27 +0300

    Update CommanderXUIX.lua

--- CommanderXUIX.lua
+++ CommanderXUIX.lua
@@ -6,11 +6,43 @@
 local RobloxGui = Instance.new("ScreenGui")
 local Frame = Instance.new("Frame")
 local UICorner = Instance.new("UICorner")
+local TextLabel = Instance.new("TextLabel")
+local UITextSizeConstraint = Instance.new("UITextSizeConstraint")
 local TextButton = Instance.new("TextButton")
 local UICorner_2 = Instance.new("UICorner")
-local UITextSizeConstraint = Instance.new("UITextSizeConstraint")
-local TextLabel = Instance.new("TextLabel")
 local UITextSizeConstraint_2 = Instance.new("UITextSizeConstraint")
+local ImageLabel = Instance.new("ImageLabel")
+local UICorner_3 = Instance.new("UICorner")
+local TextButton_2 = Instance.new("TextButton")
+local UICorner_4 = Instance.new("UICorner")
+local UITextSizeConstraint_3 = Instance.new("UITextSizeConstraint")
+local TextLabel_2 = Instance.new("TextLabel")
+local UITextSizeConstraint_4 = Instance.new("UITextSizeConstraint")
+local TextLabel_3 = Instance.new("TextLabel")
+local UITextSizeConstraint_5 = Instance.new("UITextSizeConstraint")
+local Frame_2 = Instance.new("Frame")
+local UICorner_5 = Instance.new("UICorner")
+local TextLabel_4 = Instance.new("TextLabel")
+local UITextSizeConstraint_6 = Instance.new("UITextSizeConstraint")
+local ImageLabel_2 = Instance.new("ImageLabel")
+local UICorner_6 = Instance.new("UICorner")
+local TextButton_3 = Instance.new("TextButton")
+local UICorner_7 = Instance.new("UICorner")
+local UITextSizeConstraint_7 = Instance.new("UITextSizeConstraint")
+local ImageLabel_3 = Instance.new("ImageLabel")
+local UICorner_8 = Instance.new("UICorner")
+local TextButton_4 = Instance.new("TextButton")
+local UICorner_9 = Instance.new("UICorner")
+local UITextSizeConstraint_8 = Instance.new("UITextSizeConstraint")
+local Frame_3 = Instance.new("Frame")
+local UICorner_10 = Instance.new("UICorner")
+local TextLabel_5 = Instance.new("TextLabel")
+local UITextSizeConstraint_9 = Instance.new("UITextSizeConstraint")
+local TextButton_5 = Instance.new("TextButton")
+local UICorner_11 = Instance.new("UICorner")
+local UITextSizeConstraint_10 = Instance.new("UITextSizeConstraint")
+local ImageLabel_4 = Instance.new("ImageLabel")
+local UICorner_12 = Instance.new("UICorner")
 
 --Properties:
 
@@ -22,18 +54,35 @@ Frame.Parent = RobloxGui
 Frame.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
 Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
 Frame.BorderSizePixel = 0
-Frame.Position = UDim2.new(0.408600152, 0, 0.239325076, 0)
-Frame.Size = UDim2.new(0.182427391, 0, 0.161845729, 0)
-Frame.ZIndex = 99999999999999
+Frame.Position = UDim2.new(0.226172745, 0, 0.189393938, 0)
+Frame.Size = UDim2.new(0.182427391, 0, 0.617768705, 0)
+Frame.ZIndex = 99999999999
 
 UICorner.Parent = Frame
+
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.BorderSizePixel = 0
+TextLabel.Position = UDim2.new(0, 0, 0.0206611566, 0)
+TextLabel.Size = UDim2.new(1, 0, 0.0773034766, 0)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = "الهاك العربي v1"
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 20.000
+TextLabel.TextWrapped = true
+
+UITextSizeConstraint.Parent = TextLabel
+UITextSizeConstraint.MaxTextSize = 20
 
 TextButton.Parent = Frame
 TextButton.BackgroundColor3 = Color3.fromRGB(38, 38, 38)
 TextButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
 TextButton.BorderSizePixel = 0
-TextButton.Position = UDim2.new(0.147959188, 0, 0.512708962, 0)
-TextButton.Size = UDim2.new(0.704081655, 0, 0.319148928, 0)
+TextButton.Position = UDim2.new(0.147959188, 0, 0.460918278, 0)
+TextButton.Size = UDim2.new(0.704081655, 0, 0.0781020895, 0)
 TextButton.Font = Enum.Font.SourceSans
 TextButton.Text = "تشغيل"
 TextButton.TextColor3 = Color3.fromRGB(255, 255, 255)
@@ -43,28 +92,215 @@ TextButton.TextWrapped = true
 
 UICorner_2.Parent = TextButton
 
-UITextSizeConstraint.Parent = TextButton
-UITextSizeConstraint.MaxTextSize = 20
-
-TextLabel.Parent = Frame
-TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.BackgroundTransparency = 1.000
-TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
-TextLabel.BorderSizePixel = 0
-TextLabel.Size = UDim2.new(1, 0, 0.351063818, 0)
-TextLabel.Font = Enum.Font.SourceSans
-TextLabel.Text = "الهاك العربي v1"
-TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.TextScaled = true
-TextLabel.TextSize = 20.000
-TextLabel.TextWrapped = true
-
-UITextSizeConstraint_2.Parent = TextLabel
+UITextSizeConstraint_2.Parent = TextButton
 UITextSizeConstraint_2.MaxTextSize = 20
+
+ImageLabel.Parent = Frame
+ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel.BackgroundTransparency = 1.000
+ImageLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel.BorderSizePixel = 0
+ImageLabel.Position = UDim2.new(0.244897962, 0, 0.14507252, 0)
+ImageLabel.Size = UDim2.new(0.510204077, 0, 0.278706759, 0)
+ImageLabel.Image = "rbxassetid://15832360066"
+
+UICorner_3.Parent = ImageLabel
+
+TextButton_2.Parent = Frame
+TextButton_2.BackgroundColor3 = Color3.fromRGB(38, 38, 38)
+TextButton_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_2.BorderSizePixel = 0
+TextButton_2.Position = UDim2.new(0.336734682, 0, 0.746561527, 0)
+TextButton_2.Size = UDim2.new(0.326530606, 0, 0.170075327, 0)
+TextButton_2.Font = Enum.Font.SourceSans
+TextButton_2.Text = "نسخ"
+TextButton_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextButton_2.TextScaled = true
+TextButton_2.TextSize = 20.000
+TextButton_2.TextWrapped = true
+
+UICorner_4.CornerRadius = UDim.new(0, 20)
+UICorner_4.Parent = TextButton_2
+
+UITextSizeConstraint_3.Parent = TextButton_2
+UITextSizeConstraint_3.MaxTextSize = 20
+
+TextLabel_2.Parent = Frame
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.BackgroundTransparency = 1.000
+TextLabel_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_2.BorderSizePixel = 0
+TextLabel_2.Position = UDim2.new(0, 0, 0.560931206, 0)
+TextLabel_2.Size = UDim2.new(1, 0, 0.0773034766, 0)
+TextLabel_2.Font = Enum.Font.SourceSans
+TextLabel_2.Text = "ام؟"
+TextLabel_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.TextScaled = true
+TextLabel_2.TextSize = 20.000
+TextLabel_2.TextWrapped = true
+
+UITextSizeConstraint_4.Parent = TextLabel_2
+UITextSizeConstraint_4.MaxTextSize = 20
+
+TextLabel_3.Parent = Frame
+TextLabel_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_3.BackgroundTransparency = 1.000
+TextLabel_3.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_3.BorderSizePixel = 0
+TextLabel_3.Position = UDim2.new(0, 0, 0.635968685, 0)
+TextLabel_3.Size = UDim2.new(1, 0, 0.0773034766, 0)
+TextLabel_3.Font = Enum.Font.SourceSans
+TextLabel_3.Text = "نسخ رابط سيرفر الدسكورد"
+TextLabel_3.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_3.TextScaled = true
+TextLabel_3.TextSize = 20.000
+TextLabel_3.TextWrapped = true
+
+UITextSizeConstraint_5.Parent = TextLabel_3
+UITextSizeConstraint_5.MaxTextSize = 20
+
+Frame_2.Parent = RobloxGui
+Frame_2.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
+Frame_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Frame_2.BorderSizePixel = 0
+Frame_2.Position = UDim2.new(0.408600152, 0, 0.189394131, 0)
+Frame_2.Size = UDim2.new(0.182427391, 0, 0.617768526, 0)
+
+UICorner_5.Parent = Frame_2
+
+TextLabel_4.Parent = Frame_2
+TextLabel_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_4.BackgroundTransparency = 1.000
+TextLabel_4.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_4.BorderSizePixel = 0
+TextLabel_4.Position = UDim2.new(0, 0, 0.00151042046, 0)
+TextLabel_4.Size = UDim2.new(1, 0, 0.0724139065, 0)
+TextLabel_4.Font = Enum.Font.SourceSans
+TextLabel_4.Text = "شكل الهاك؟"
+TextLabel_4.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_4.TextScaled = true
+TextLabel_4.TextSize = 20.000
+TextLabel_4.TextWrapped = true
+
+UITextSizeConstraint_6.Parent = TextLabel_4
+UITextSizeConstraint_6.MaxTextSize = 20
+
+ImageLabel_2.Parent = Frame_2
+ImageLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel_2.BackgroundTransparency = 1.000
+ImageLabel_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel_2.BorderSizePixel = 0
+ImageLabel_2.Position = UDim2.new(0, 0, 0.558956444, 0)
+ImageLabel_2.Size = UDim2.new(1, 0, 0.309162319, 0)
+ImageLabel_2.Image = "rbxassetid://15847088931"
+
+UICorner_6.Parent = ImageLabel_2
+
+TextButton_3.Parent = Frame_2
+TextButton_3.BackgroundColor3 = Color3.fromRGB(38, 38, 38)
+TextButton_3.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_3.BorderSizePixel = 0
+TextButton_3.Position = UDim2.new(0.147959188, 0, 0.884841144, 0)
+TextButton_3.Size = UDim2.new(0.704081655, 0, 0.0819627568, 0)
+TextButton_3.Font = Enum.Font.SourceSans
+TextButton_3.Text = "تشغيل"
+TextButton_3.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextButton_3.TextScaled = true
+TextButton_3.TextSize = 20.000
+TextButton_3.TextWrapped = true
+
+UICorner_7.Parent = TextButton_3
+
+UITextSizeConstraint_7.Parent = TextButton_3
+UITextSizeConstraint_7.MaxTextSize = 20
+
+ImageLabel_3.Parent = Frame_2
+ImageLabel_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel_3.BackgroundTransparency = 1.000
+ImageLabel_3.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel_3.BorderSizePixel = 0
+ImageLabel_3.Position = UDim2.new(0, 0, 0.0711872354, 0)
+ImageLabel_3.Size = UDim2.new(1, 0, 0.346107185, 0)
+ImageLabel_3.Image = "rbxassetid://15847106687"
+
+UICorner_8.Parent = ImageLabel_3
+
+TextButton_4.Parent = Frame_2
+TextButton_4.BackgroundColor3 = Color3.fromRGB(38, 38, 38)
+TextButton_4.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_4.BorderSizePixel = 0
+TextButton_4.Position = UDim2.new(0.147959188, 0, 0.457221895, 0)
+TextButton_4.Size = UDim2.new(0.704081655, 0, 0.0722438246, 0)
+TextButton_4.Font = Enum.Font.SourceSans
+TextButton_4.Text = "تشغيل"
+TextButton_4.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextButton_4.TextScaled = true
+TextButton_4.TextSize = 20.000
+TextButton_4.TextWrapped = true
+
+UICorner_9.Parent = TextButton_4
+
+UITextSizeConstraint_8.Parent = TextButton_4
+UITextSizeConstraint_8.MaxTextSize = 20
+
+Frame_3.Parent = RobloxGui
+Frame_3.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
+Frame_3.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Frame_3.BorderSizePixel = 0
+Frame_3.Position = UDim2.new(0.591027558, 0, 0.189393952, 0)
+Frame_3.Size = UDim2.new(0.182427391, 0, 0.332989961, 0)
+
+UICorner_10.Parent = Frame_3
+
+TextLabel_5.Parent = Frame_3
+TextLabel_5.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_5.BackgroundTransparency = 1.000
+TextLabel_5.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_5.BorderSizePixel = 0
+TextLabel_5.Position = UDim2.new(0, 0, 0.0206611566, 0)
+TextLabel_5.Size = UDim2.new(1, 0, 0.12258669, 0)
+TextLabel_5.Font = Enum.Font.SourceSans
+TextLabel_5.Text = "شكل الهاك 2"
+TextLabel_5.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_5.TextScaled = true
+TextLabel_5.TextSize = 20.000
+TextLabel_5.TextWrapped = true
+
+UITextSizeConstraint_9.Parent = TextLabel_5
+UITextSizeConstraint_9.MaxTextSize = 20
+
+TextButton_5.Parent = Frame_3
+TextButton_5.BackgroundColor3 = Color3.fromRGB(38, 38, 38)
+TextButton_5.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_5.BorderSizePixel = 0
+TextButton_5.Position = UDim2.new(0.147959188, 0, 0.843075812, 0)
+TextButton_5.Size = UDim2.new(0.704081655, 0, 0.134027943, 0)
+TextButton_5.Font = Enum.Font.SourceSans
+TextButton_5.Text = "تشغيل"
+TextButton_5.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextButton_5.TextScaled = true
+TextButton_5.TextSize = 20.000
+TextButton_5.TextWrapped = true
+
+UICorner_11.Parent = TextButton_5
+
+UITextSizeConstraint_10.Parent = TextButton_5
+UITextSizeConstraint_10.MaxTextSize = 20
+
+ImageLabel_4.Parent = Frame_3
+ImageLabel_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel_4.BackgroundTransparency = 1.000
+ImageLabel_4.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel_4.BorderSizePixel = 0
+ImageLabel_4.Position = UDim2.new(0, 0, 0.139585286, 0)
+ImageLabel_4.Size = UDim2.new(1, 0, 0.624245465, 0)
+ImageLabel_4.Image = "rbxassetid://15847081527"
+
+UICorner_12.Parent = ImageLabel_4
 
 -- Scripts:
 
-local function UBKWN_fake_script() -- TextButton.LocalScript 
+local function GKAHK_fake_script() -- TextButton.LocalScript 
 	local script = Instance.new('LocalScript', TextButton)
 
 	script.Parent.MouseButton1Click:Connect(function()
@@ -72,4 +308,40 @@ local function UBKWN_fake_script() -- TextButton.LocalScript
 		loadstring(game:HttpGet('https://raw.githubusercontent.com/anyahubs/CommanderX/main/Commander.lua'))()
 	end)
 end
-coroutine.wrap(UBKWN_fake_script)()
+coroutine.wrap(GKAHK_fake_script)()
+local function GMCBI_fake_script() -- TextButton_2.LocalScript 
+	local script = Instance.new('LocalScript', TextButton_2)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		script.Parent.Parent.Parent:Destroy()
+		setclipboard("https://discord.gg/zrrSXcTa5t")
+	end)
+end
+coroutine.wrap(GMCBI_fake_script)()
+local function KDJLCE_fake_script() -- TextButton_3.LocalScript 
+	local script = Instance.new('LocalScript', TextButton_3)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		script.Parent.Parent.Parent:Destroy()
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/anyahubs/CommanderX/main/Commander.lua'))()
+	end)
+end
+coroutine.wrap(KDJLCE_fake_script)()
+local function RGAN_fake_script() -- TextButton_4.LocalScript 
+	local script = Instance.new('LocalScript', TextButton_4)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		script.Parent.Parent.Parent:Destroy()
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/anyahubs/CommanderX/main/CommanderXUI'))()
+	end)
+end
+coroutine.wrap(RGAN_fake_script)()
+local function ABNBWS_fake_script() -- TextButton_5.LocalScript 
+	local script = Instance.new('LocalScript', TextButton_5)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		script.Parent.Parent.Parent:Destroy()
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/anyahubs/CommanderX/main/CommanderXUIZ.lua'))()
+	end)
+end
+coroutine.wrap(ABNBWS_fake_script)()
