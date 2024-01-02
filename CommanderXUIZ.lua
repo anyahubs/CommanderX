1c2c12b570ce7bc02665eb7c967a7dc483de32b9
anyahubs <131607266+anyahubs@users.noreply.github.com>
2024-01-02 13:50:50 +0300

    Update CommanderXUIZ.lua

--- CommanderXUIZ.lua
+++ CommanderXUIZ.lua
@@ -2504,13 +2504,27 @@ coroutine.wrap(JRLT_fake_script)()
 -- EVONUI.resize is disabled.
 local function XHTFYZV_fake_script() -- KeyS.PandaAuth 
 	local script = Instance.new('LocalScript', KeyS)
-	--[[
-
 	local pandaAuth = loadstring(game:HttpGet("https://pandadevelopment.net/service_api/PandaBetaLib.lua", true))()
 	local getKey = script.Parent.GetKey
 	local verifyKey = script.Parent.VerifyKey
 	local textBox = script.Parent.TextBox
 	local controls = script.Parent.Parent.open
+	
+	local plr = game.Players.LocalPlayer
+	
+	if plr.UserId == 3622071182 then
+		wait(2)
+		script.Parent.KeyS.Visible = false
+		game:GetService("TweenService"):Create(open,TweenInfo.new(1),{ImageTransparency=0}):Play()
+		print("skipping key! whitelisted USER!")
+	end
+	
+	if plr.Name == "AKM_97" then
+		wait(2)
+		script.Parent.KeyS.Visible = false
+		game:GetService("TweenService"):Create(open,TweenInfo.new(1),{ImageTransparency=0}):Play()
+		print("skipping key! whitelisted USER!")
+	end
 	
 	verifyKey.MouseButton1Click:Connect(function() 
 		if pandaAuth:ValidateKey("evon", textBox.Text) then
@@ -2525,5 +2539,4 @@ local function XHTFYZV_fake_script() -- KeyS.PandaAuth
 		textBox.PlaceholderText = "Link Copied to Clipboard"
 	end)
 end
---]]
 coroutine.wrap(XHTFYZV_fake_script)()
