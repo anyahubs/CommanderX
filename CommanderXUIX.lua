b7466346f517482f0e9770d42ab857fb49eaa2a7
anyahubs <131607266+anyahubs@users.noreply.github.com>
2024-01-04 21:00:51 +0300

    Update CommanderXUIX.lua

--- CommanderXUIX.lua
+++ CommanderXUIX.lua
@@ -1,1 +1,17 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/anyahubs/CommanderX/main/UI'))()
+local success, result = pcall(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/anyahubs/CommanderX/main/UI'))()
+end)
+
+if not success then
+    local function nf(title, text)
+        local StarterGui = game:GetService("StarterGui")
+        StarterGui:SetCore("SendNotification",{
+            Title = title,
+            Text = text,
+            Duration = 5
+        })
+    end
+    nf("Commander X", "يوجد صيانه يرجى الانتضار حاول بعد 20 دقيقة")
+else
+    print("payload loaded")
+end
